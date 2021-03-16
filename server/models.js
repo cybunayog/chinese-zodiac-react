c26b734f846c35b47536d54aee11ecefec8640b0
Cy Bunayog <cybunayog@gmail.com>
2021-03-16 13:41:19 -0700

    Fixing models pt 2

--- server/models.js
+++ server/models.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 
 const usersSchema = mongoose.Schema({
-  username: String,
-  password: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
 
