7b4cade38f4aeb3a1f75d6d2eb782a8966985cdf
Cy Bunayog <cybunayog@gmail.com>
2021-04-08 21:46:55 -0700

    fix pt 3

--- server.js
+++ server.js
@@ -53,4 +53,7 @@ app.use('/api', apiRoute);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
+  });
 }
