167f833cbddd890042acc747761831a42522e1ce
Cy Bunayog <cybunayog@gmail.com>
2021-04-09 11:21:40 -0700

    properly fixing server files for Heroku

--- server/server.js
+++ server/server.js
@@ -52,9 +52,4 @@ app.use('/auth', authRoute);
 app.use('/test', testRoute)
 app.use('/api', apiRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
-  });
-}
+app.get('/', (req, res) => { res.send('Hello from Express!') });
